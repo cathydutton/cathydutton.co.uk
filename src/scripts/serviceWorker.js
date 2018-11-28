437e69f58dfd3518d94b5fdb7d6cea0dcf1bb009
cathydutton <info@cathydutton.co.uk>
2018-11-28 16:52:37 +0000

    Update service worker version

--- src/scripts/serviceWorker.js
+++ src/scripts/serviceWorker.js
@@ -7,7 +7,7 @@
 
     // Update 'version' if you need to refresh the cache
     var staticCacheName = 'static';
-    var version = 'v1::';
+    var version = 'v2::';
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
