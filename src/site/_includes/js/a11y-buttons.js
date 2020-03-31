37786fa3609a905440a40e123b248fdbbf6ae7df
cathydutton <info@cathydutton.co.uk>
2020-03-31 21:08:15 +0100

    Add favicon

--- src/site/_includes/js/a11y-buttons.js
+++ src/site/_includes/js/a11y-buttons.js
@@ -11,9 +11,9 @@
 
   
   // FONT
-  // If no local storage value exists create one
+  // If no session storage value exists create one
   if (!fontFamily) {
-      body.style.setProperty('font-family', localStorage.getItem('fontFamily'));
+      body.style.setProperty('font-family', sessionStorage.getItem('fontFamily'));
   }
 
   // Get current value
@@ -28,7 +28,7 @@
     body.style.setProperty('font-family', '"OpenDyslexic", sans-serif');
     defaultFont.classList.remove("button--ally--active");
     dyslexicFont.classList.add("button--ally--active");
-    localStorage.setItem("fontFamily", '"OpenDyslexic", sans-serif');
+    sessionStorage.setItem("fontFamily", '"OpenDyslexic", sans-serif');
   });
 
   // Default font family
@@ -36,7 +36,7 @@
     body.style.setProperty('font-family', 'Verdana, Geneva, sans-serif');
     dyslexicFont.classList.remove("button--ally--active");
     defaultFont.classList.add("button--ally--active");
-    localStorage.setItem("fontFamily", 'Verdana, Geneva, sans-serif');
+    sessionStorage.setItem("fontFamily", 'Verdana, Geneva, sans-serif');
   });
 
 
@@ -47,14 +47,14 @@
   // var themeContrast = document.getElementById('theme-contrast');
   var themeMonochrome = document.getElementById('theme-monochrome');
 
-  body.className = localStorage.getItem('currentTheme');
+  body.className = sessionStorage.getItem('currentTheme');
 
   themeDefault.addEventListener('click', function (e) {
     body.className = 'theme-default';
     themeDefault.classList.add("button--ally--active");
     themeMonochrome.classList.remove("button--ally--active");
     // themeContrast.classList.remove("button--ally--active");
-    localStorage.setItem("currentTheme", 'theme-default');
+    sessionStorage.setItem("currentTheme", 'theme-default');
   });
 
   // themeContrast.addEventListener('click', function (e) {
@@ -62,7 +62,7 @@
   //   themeContrast.classList.add("button--ally--active");
   //   themeMonochrome.classList.remove("button--ally--active");
   //   themeDefault.classList.remove("button--ally--active");
-  //   localStorage.setItem('currentTheme', 'theme-contrast');
+  //   sessionStorage.setItem('currentTheme', 'theme-contrast');
   // });
 
   themeMonochrome.addEventListener('click', function (e) {
@@ -70,7 +70,7 @@
     themeMonochrome.classList.add("button--ally--active");
     // themeContrast.classList.remove("button--ally--active");
     themeDefault.classList.remove("button--ally--active");
-    localStorage.setItem('currentTheme', 'theme-monochrome');
+    sessionStorage.setItem('currentTheme', 'theme-monochrome');
   });
 
 })();
