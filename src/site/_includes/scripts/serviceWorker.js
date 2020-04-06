227f2483d5af0106d81938c00429b3e29659fbdc
cathydutton <info@cathydutton.co.uk>
2020-04-06 17:32:06 +0100

    manifest.jsn

--- src/site/_includes/scripts/serviceWorker.js
+++ src/site/_includes/scripts/serviceWorker.js
@@ -7,7 +7,7 @@
 
     // Update 'version' if you need to refresh the cache
     var staticCacheName = 'static';
-    var version = 'v4::';
+    var version = 'v5::';
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
