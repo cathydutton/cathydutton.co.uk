455c4e58a1b027c35e3f352d4c4ee206d6fe8c82
cathydutton <info@cathydutton.co.uk>
2020-04-06 19:28:09 +0100

    Fix top buttons

--- .eleventy.js
+++ .eleventy.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
         removeComments: true,
         collapseWhitespace: true,
       })
-      //return minified
+      return minified
     }
     return content
   })
--- src/site/_includes/scripts/serviceWorker.js
+++ src/site/_includes/scripts/serviceWorker.js
@@ -7,7 +7,7 @@
 
     // Update 'version' if you need to refresh the cache
     var staticCacheName = 'static';
-    var version = 'v5::';
+    var version = 'v6::';
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
