9f904fda32d4616694a935b4e7e66358c9b16bd6
cathydutton <info@cathydutton.co.uk>
2020-04-11 15:40:48 +0100

    Share button

--- src/site/_includes/js/a11y-buttons.js
+++ src/site/_includes/js/a11y-buttons.js
@@ -9,25 +9,37 @@
   // GLOBAL
   var body = document.body;
 
-  
+ 
   // FONT
   // If no session storage value exists create one
   if (!fontFamily) {
-      body.style.setProperty('font-family', sessionStorage.getItem('fontFamily'));
+    body.style.setProperty('font-family', sessionStorage.getItem('fontFamily')); 
   }
+
+  // if (document.getElementById('dyslexic-font').classList.contains('null')) {
+  //   sessionStorage.setItem("defaultFontClass", 'button--ally--active');
+  // }
+
 
   // Get current value
   var fontFamily = getComputedStyle(body).getPropertyValue('font-family');
 
+
   // Set buttons
   var dyslexicFont = document.getElementById('dyslexic-font');
   var defaultFont = document.getElementById('default-font');
+
+  defaultFont.className = sessionStorage.getItem('defaultFontClass');
+  dyslexicFont.className = sessionStorage.getItem('dyslexicFontClass');
+
 
   // Dyslexic font family
   dyslexicFont.addEventListener('click', function (e) {
     body.style.setProperty('font-family', '"OpenDyslexic", sans-serif');
     defaultFont.classList.remove("button--ally--active");
     dyslexicFont.classList.add("button--ally--active");
+    sessionStorage.setItem("dyslexicFontClass", 'button--ally--active');
+    sessionStorage.removeItem("defaultFontClass");
     sessionStorage.setItem("fontFamily", '"OpenDyslexic", sans-serif');
   });
 
@@ -36,6 +48,8 @@
     body.style.setProperty('font-family', 'Verdana, Geneva, sans-serif');
     dyslexicFont.classList.remove("button--ally--active");
     defaultFont.classList.add("button--ally--active");
+    sessionStorage.setItem("defaultFontClass", 'button--ally--active');
+    sessionStorage.removeItem("dyslexicFontClass");
     sessionStorage.setItem("fontFamily", 'Verdana, Geneva, sans-serif');
   });
 
@@ -44,32 +58,30 @@
 
   // Set buttons
   var themeDefault = document.getElementById('theme-default');
-  // var themeContrast = document.getElementById('theme-contrast');
   var themeMonochrome = document.getElementById('theme-monochrome');
+
+  themeDefault.className = sessionStorage.getItem('themeDefaultClass');
+  themeMonochrome.className = sessionStorage.getItem('themeMonochromeClass');
 
   body.className = sessionStorage.getItem('currentTheme');
 
   themeDefault.addEventListener('click', function (e) {
     body.className = 'theme-default';
-    themeDefault.classList.add("button--ally--active");
-    themeMonochrome.classList.remove("button--ally--active");
-    // themeContrast.classList.remove("button--ally--active");
+    themeDefault.className = 'button--ally--active';
+    themeMonochrome.className = '';
+    sessionStorage.setItem("themeDefaultClass", 'button--ally--active');
+    sessionStorage.setItem("themeMonochromeClass", '');
     sessionStorage.setItem("currentTheme", 'theme-default');
   });
 
-  // themeContrast.addEventListener('click', function (e) {
-  //   body.className = 'theme-contrast';
-  //   themeContrast.classList.add("button--ally--active");
-  //   themeMonochrome.classList.remove("button--ally--active");
-  //   themeDefault.classList.remove("button--ally--active");
-  //   sessionStorage.setItem('currentTheme', 'theme-contrast');
-  // });
+
 
   themeMonochrome.addEventListener('click', function (e) {
     body.className = 'theme-monochrome';
-    themeMonochrome.classList.add("button--ally--active");
-    // themeContrast.classList.remove("button--ally--active");
-    themeDefault.classList.remove("button--ally--active");
+    themeDefault.className = '';
+    themeMonochrome.className = 'button--ally--active';
+    sessionStorage.setItem("themeDefaultClass", '');
+    sessionStorage.setItem("themeMonochromeClass", 'button--ally--active');
     sessionStorage.setItem('currentTheme', 'theme-monochrome');
   });
 
