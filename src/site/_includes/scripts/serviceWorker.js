efaecce62285836efd9736dd41e3b23c0c65f771
cathydutton <info@cathydutton.co.uk>
2020-05-24 17:42:16 +0100

    New post

--- src/site/_includes/scripts/serviceWorker.js
+++ src/site/_includes/scripts/serviceWorker.js
@@ -7,7 +7,7 @@
 
     // Update 'version' if you need to refresh the cache
     var staticCacheName = 'static';
-    var version = 'v13::';
+    var version = 'v14::';
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
